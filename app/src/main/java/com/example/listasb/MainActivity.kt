ea5a435e625c9a38fb116553c77e23fb3ae82ca5
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2025-10-21 12:18:26 -0500

    Initial commit.

---
+++ app/src/main/java/com/example/listasb/MainActivity.kt
@@ -0,0 +1,469 @@
+package com.example.listasb
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.listasb.ui.theme.ListasBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListasBTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding -> AppContent(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppContent(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginContent(navController, modifier) }
+        composable("menu") { MenuContent(navController, modifier) }
+        composable("lstProductos") { LstProductosContent(navController, modifier) }
+        composable("frmProductos") { frmProductosContent(navController, modifier) }
+        composable("ventas") { LstVentasContent(navController, modifier) }
+    }
+}
+
+@Composable
+fun LoginContent(navController: NavHostController, modifier: Modifier) {
+    val context = LocalContext.current
+
+    var usuario: String by remember { mutableStateOf("") }
+    var contrasena: String by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Button(
+            onClick = {
+                navController.navigate("menu")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Menú",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Text(
+            text = "Inicio de Sesión",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Usuario:")
+        TextField(
+            value = usuario,
+            onValueChange = { usuario = it },
+            placeholder = { Text("Ingresa tu usuario") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Contraseña:")
+        TextField(
+            value = contrasena,
+            onValueChange = { contrasena = it },
+            placeholder = { Text("Ingresa tu contraseña") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {
+                Toast.makeText(context, "Usuario: ${usuario}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Contraseña: ${contrasena}", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Iniciar sesión")
+        }
+
+
+    }
+}
+
+@Composable
+fun MenuContent(navController: NavHostController, modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Menú",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("login")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Login",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("productos")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Productos",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("ventas")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Ventas",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+    }
+}
+
+@Composable
+fun LstProductosContent(navController: NavHostController, modifier: Modifier) {
+    data class Producto(val nombre: String, val precio: Double, val existencias: Int)
+    val productos = remember {
+        mutableStateListOf(
+            Producto("Sponch Fresa", 23.0, 10),
+            Producto("Emperador Combinado", 22.0, 10),
+            Producto("Florentinas Cajeta", 20.0, 8)
+        )
+    }
+    // productos[2] = Producto("Florentinas Fresa", 20.0, 5)
+
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .horizontalScroll(scrollState)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Button(
+            onClick = {
+                navController.navigate("menu")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Menú",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("frmProductos")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Formulario",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(
+            onClick = {
+                productos.add(Producto("Canelitas", 21.0, 12))
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Agregar Producto Prueba",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Text("Nombre", modifier = Modifier.width(150.dp), fontWeight = FontWeight.Bold)
+            Text("Precio", modifier = Modifier.width(100.dp), fontWeight = FontWeight.Bold)
+            Text("Existencias", modifier = Modifier.width(100.dp), fontWeight = FontWeight.Bold)
+            Text("Eliminar", modifier = Modifier.width(100.dp), fontWeight = FontWeight.Bold)
+        }
+        Divider()
+        productos.forEachIndexed { index, producto ->
+            val bgColor = if (index % 2 == 0) Color(0xFFF5F5F5) else Color.White
+
+            Row (
+                modifier = Modifier
+                    .background(bgColor)
+            ) {
+                Text(producto.nombre, modifier = Modifier
+                    .width(150.dp)
+                )
+                Text("$ ${producto.precio}", modifier = Modifier
+                    .width(100.dp)
+                )
+                Text("x ${producto.existencias}", modifier = Modifier
+                    .width(100.dp)
+                )
+                Button(onClick = {
+                    productos.removeAt(index)
+                }) {
+                    Text("Eliminar")
+                }
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun frmProductosContent(navController: NavHostController, modifier: Modifier) {
+    val context = LocalContext.current
+
+    var nombre: String by remember { mutableStateOf("") }
+    var precio: String by remember { mutableStateOf("") }
+    var existencias: String by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Button(
+            onClick = {
+                navController.navigate("lstProductos")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Tabla",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Text(text = "Nombre:")
+        TextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            placeholder = { Text("Ingresa el nombre") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Precio:")
+        TextField(
+            value = precio,
+            onValueChange = { precio = it },
+            placeholder = { Text("Ingresa el precio") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Existencias:")
+        TextField(
+            value = existencias,
+            onValueChange = { existencias = it },
+            placeholder = { Text("Ingresa las existencias") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                Toast.makeText(context, "Nombre: ${nombre}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Precio: ${precio}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Existencias: ${existencias}", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Enviar")
+        }
+
+    }
+}
+
+@Composable
+fun LstVentasContent(navController: NavHostController, modifier: Modifier) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .horizontalScroll(scrollState)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Button(
+            onClick = {
+                navController.navigate("productos")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Productos",
+                style = TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppContentPreview() {
+    ListasBTheme {
+        AppContent()
+    }
+}
